d313cf82afcfe8b3b1c2a95a349aee57bea058ae
Althrone <imitatores131718@icloud.com>
2020-07-27 17:58:38 +0800

    master

--- source/chip/stm8s103/inc/memory_map.h
+++ source/chip/stm8s103/inc/memory_map.h
@@ -3,3 +3,28 @@
  * 按照芯片数据手册进行各类宏定义
  ****************************************************************************/
 
+#define RAM_BASE	0x00000000 /**/
+
+#define PA_ODR *(volatile unsigned char *)0x5000
+#define PA_IDR *(volatile unsigned char *)0x5001
+#define PA_DDR *(volatile unsigned char *)0x5002
+#define PA_CR1 *(volatile unsigned char *)0x5003
+#define PA_CR2 *(volatile unsigned char *)0x5004
+
+#define PB_ODR *(volatile unsigned char *)0x5005
+#define PB_IDR *(volatile unsigned char *)0x5006
+#define PB_DDR *(volatile unsigned char *)0x5007
+#define PB_CR1 *(volatile unsigned char *)0x5008
+#define PB_CR2 *(volatile unsigned char *)0x5009
+
+#define PC_ODR *(volatile unsigned char *)0x500A
+#define PC_IDR *(volatile unsigned char *)0x500B
+#define PC_DDR *(volatile unsigned char *)0x500C
+#define PC_CR1 *(volatile unsigned char *)0x500D
+#define PC_CR2 *(volatile unsigned char *)0x500E
+
+#define PD_ODR *(volatile unsigned char *)0x500F
+#define PD_IDR *(volatile unsigned char *)0x5010
+#define PD_DDR *(volatile unsigned char *)0x5011
+#define PD_CR1 *(volatile unsigned char *)0x5012
+#define PD_CR2 *(volatile unsigned char *)0x5013
---
+++ source/include/config.h
@@ -0,0 +1,1 @@
+#define CONFIG_MICROCONTROLLER_BIT 32   //定义单片机为32位机，以后放到.config中，使用make menuconfig设置这些功能
