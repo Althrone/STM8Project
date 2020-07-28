7ef6aa75d5a28c3f6c1fda777440a039cd4e4976
Althrone <imitatores131718@icloud.com>
2020-07-28 16:10:47 +0800

    master

--- source/chip/stm8s103/inc/memory_map.h
+++ source/chip/stm8s103/inc/memory_map.h
@@ -8,15 +8,47 @@
 
 #include "../../../include/config.h"
 
-#define RAM_BASE	0x00000000 /**/
+#define PA_BASE			0x005000/* 0x005000-0x005004: Port A                    */
+#define PB_BASE			0x005005/* 0x005005-0x005009: Port B                    */
+#define PC_BASE			0x00500A/* 0x00500A-0x00500E: Port C                    */
+#define PD_BASE			0x00500F/* 0x00500F-0x005013: Port D                    */
+#define PE_BASE			0x005014/* 0x005014-0x005018: Port E                    */
+#define PF_BASE			0x005019/* 0x005019-0x00501D: Port F                    */
+                                /* 0x00501E-0x005059: Reserved area (60 byte)   */
+#define FLASH_BASE      0x00505A/* 0x00505A-0x005064: Flash                     */
+                                /* 0x005065-0x00509F: Reserved area (59 byte)   */
+#define EXTI_BASE       0x0050A0/* 0x0050A0-0x0050A1: EXTI                      */
+                                /* 0x0050A2-0x0050B2: Reserved area (17 byte)   */
+#define RST_BASE        0x0050B3/* 0x0050B3-0x0050B3: RST                       */
+                                /* 0x0050B4-0x0050BF: Reserved area (12 byte)   */
+#define CLK_BASE        0x0050C0/* 0x0050C0-0x0050CD: CLK                       */
+                                /* 0x0050CE-0x0050D0: Reserved area (3 byte)    */
+#define WWDG_BASE       0x0050D1/* 0x0050D1-0x0050D2: WWDG                      */
+                                /* 0x0050D3-0x0050DF: Reserved area (13 byte)   */
+#define IWDG_BASE       0x0050E0/* 0x0050E0-0x0050E2: IWDG                      */
+                                /* 0x0050E3-0x0050EF: Reserved area (13 byte)   */
+#define AWU_BASE        0x0050F0/* 0x0050F0-0x0050F2: AWU                       */
+#define BEEP_BASE       0x0050F3/* 0x0050F3-0x0050F3: BEEP                      */
+                                /* 0x0050F4-0x0050FF: Reserved area (12 byte)   */
+                                /* 0x005100-0x0051FF: Reserved area (265 byte)  */
+#define SPI_BASE        0x005200/* 0x005200-0x0050F3: SPI                       */
 
-#define PA_BASE			0x005000
-#define PB_BASE			0x005005
-#define PC_BASE			0x00500A
-#define PD_BASE			0x00500F
-#define PE_BASE			0x005014
-#define PF_BASE			0x005019
 
+
+
+
+                                /* 0x005410-0x0057FF: Reserved area (1008 byte) */
+
+
+#define CPU_BASE        0X007F60/* 0X007F60-0X007F60: CPU                       */
+                                /* 0x007F61-0x007F6F: Reserved area (2 byte)    */
+#define ITC_BASE        0x007F70/* 0x007F70-0x007F77: ITC                       */
+                                /* 0x007F78-0x007F79: Reserved area (2 byte)    */
+#define SWIM_BASE       0x007F80/* 0x007F80-0x007F80: SWIM                      */
+                                /* 0x007F81-0x007F8F: Reserved area (15 byte)   */
+#define DM_BASE         0x007F90/* 0x007F90-0x007F9A: DM                        */
 
 
 #endif /*_MEMORY_MAPE_H_*/
+
+/**/
--- source/chip/stm8s103/inc/uart.h
+++ source/chip/stm8s103/inc/uart.h
@@ -1,11 +1,32 @@
-#ifndef _UART_H_
-#define _UART_H_
+#ifndef _UARTX_H_
+#define _UARTX_H_
 
 #include "./memory_map.h"
 
+/**
+ * ┌ └ ┐ ┘ ─ │ ├ ┤ ┬ ┴ ┼
+ * ┌
+ * │
+ * ├
+ * 
+ */
+
 //寄存器偏移地址
-                        //
+
+#define UARTX_SR_OFFSET         0x0000
+#define UARTX_DR_OFFSET         0x0001
+#define UARTX_BRR1_OFFSET       0x0002
+#define UARTX_BRR2_OFFSET       0x0003
+#define UARTX_CR1_OFFSET        0x0004
+#define UARTX_CR2_OFFSET        0x0005
+#define UARTX_CR3_OFFSET        0x0006
+#define UARTX_CR4_OFFSET        0x0007
+#define UARTX_CR5_OFFSET        0x0008
+#define UARTX_CR6_OFFSET        0x0009
+
+
 //寄存器操作
+
 #define UARTX_SR_PE             (1<<0)
 #define UARTX_SR_FE             (1<<1)
 #define UARTX_SR_NF             (1<<2)
@@ -30,44 +51,76 @@
 #define UARTX_BRR2_MSB_MASK     (0xF<<UARTX_BRR2_MSB_SHIFT)
     #define UARTX_BRR2_MSB_(n)  ((n)<<UARTX_BRR2_MSB_SHIFT)
 
-#define UART_CR1_PIEN           (1<<0)
-#define UART_CR1_PS             (1<<1)
-#define UART_CR1_PCEN           (1<<2)
-#define UART_CR1_WAKE           (1<<3)
-#define UART_CR1_M              (1<<4)
-#define UART_CR1_UARTD          (1<<5)
-#define UART_CR1_T8             (1<<6)
-#define UART_CR1_R8             (1<<7)
+#define UARTX_CR1_PIEN          (1<<0)
+#define UARTX_CR1_PS            (1<<1)
+#define UARTX_CR1_PCEN          (1<<2)
+#define UARTX_CR1_WAKE          (1<<3)
+#define UARTX_CR1_M             (1<<4)
+#define UARTX_CR1_UARTD         (1<<5)
+#define UARTX_CR1_T8            (1<<6)
+#define UARTX_CR1_R8            (1<<7)
 
-#define UART_CR2_SBK            (1<<0)
-#define UART_CR2_RWU            (1<<1)
-#define UART_CR2_REN            (1<<2)
-#define UART_CR2_TEN            (1<<3)
-#define UART_CR2_ILIEN          (1<<4)
-#define UART_CR2_RIEN           (1<<5)
-#define UART_CR2_TCIEN          (1<<6)
-#define UART_CR2_TIEN           (1<<7)
+#define UARTX_CR2_SBK           (1<<0)
+#define UARTX_CR2_RWU           (1<<1)
+#define UARTX_CR2_REN           (1<<2)
+#define UARTX_CR2_TEN           (1<<3)
+#define UARTX_CR2_ILIEN         (1<<4)
+#define UARTX_CR2_RIEN          (1<<5)
+#define UARTX_CR2_TCIEN         (1<<6)
+#define UARTX_CR2_TIEN          (1<<7)
 
-#define UART_CR3_LBCL           (1<<0)
-#define UART_CR3_CPHA           (1<<1)
-#define UART_CR3_CPOL           (1<<2)
-#define UART_CR3_CLKEN          (1<<3)
-#define UART_CR3_STOP_SHIFT     (4)
-#define UART_CR3_STOP_MASK      (0x3<<UART_CR3_STOP_SHIFT)
-    #define UART_CR3_STOP_1     (0x0<<UART_CR3_STOP_SHIFT)
-  //#define UART_CR3_STOP_Res   (0x1<<UART_CR3_STOP_SHIFT)
-    #define UART_CR3_STOP_2     (0x2<<UART_CR3_STOP_SHIFT)
-    #define UART_CR3_STOP_1P5   (0x3<<UART_CR3_STOP_SHIFT)
-#define UART_CR3_STOP1          (1<<4)
-#define UART_CR3_STOP2          (1<<5)
-#define UART_CR3_LINEN          (1<<6)
-//#define UART_CR3_Res          (1<<7)
+#define UARTX_CR3_LBCL          (1<<0)
+#define UARTX_CR3_CPHA          (1<<1)
+#define UARTX_CR3_CPOL          (1<<2)
+#define UARTX_CR3_CLKEN         (1<<3)
+#define UARTX_CR3_STOP_SHIFT    (4)
+#define UARTX_CR3_STOP_MASK     (0x3<<UARTX_CR3_STOP_SHIFT)
+    #define UARTX_CR3_STOP_1    (0x0<<UARTX_CR3_STOP_SHIFT)
+  //#define UARTX_CR3_STOP_Res  (0x1<<UARTX_CR3_STOP_SHIFT)
+    #define UARTX_CR3_STOP_2    (0x2<<UARTX_CR3_STOP_SHIFT)
+    #define UARTX_CR3_STOP_1P5  (0x3<<UARTX_CR3_STOP_SHIFT)
+#define UARTX_CR3_LINEN         (1<<6)
+//#define UARTX_CR3_Res         (1<<7)
+
+#define UARTX_CR4_ADD_SHIFT     (0)
+#define UARTX_CR4_ADD_MASK      (0xF<<UARTX_CR4_ADD_SHIFT)
+    #define UARTX_CR4_ADD_(n)   ((n)<<UARTX_CR4_ADD_SHIFT)
+#define UARTX_CR4_LBDF          (1<<4)
+#define UARTX_CR4_LBDL          (1<<5)
+#define UARTX_CR4_LBDIEN        (1<<6)
+//#define UARTX_CR4_Res         (1<<7)
+
+//#define UARTX_CR5_Res         (1<<0)
+#define UARTX_CR5_IREN          (1<<1)
+#define UARTX_CR5_IRLP          (1<<2)
+#define UARTX_CR5_HDSEL         (1<<3)
+#define UARTX_CR5_NACK          (1<<4)
+#define UARTX_CR5_SCEN          (1<<5)
+//#define UARTX_CR5_Res         (1<<6)
+//#define UARTX_CR5_Res         (1<<7)
+
+#define UARTX_CR_LSF            (1<<0)
+#define UARTX_CR_LHDF           (1<<1)
+#define UARTX_CR_LHDIEN         (1<<2)
+//#define UARTX_CR_Res          (1<<3)
+#define UARTX_CR_LASE           (1<<4)
+#define UARTX_CR_LSLV           (1<<5)
+//#define UARTX_CR_Res          (1<<6)
+#define UARTX_CR_LDUM           (1<<7)
+
+#define UARTX_GTR_GT_SHIFT      (0)
+#define UARTX_GTR_GT_MASK       (0xFF<<UARTX_GTR_GT_SHIFT)
+    #define UARTX_GTR_GT_(n)    ((n)<<UARTX_GTR_GT_SHIFT)
+
+#define UARTX_PSCR_PSC_SHIFT    (0)
+#define UARTX_PSCR_PSC_MASK     (0xFF<<UARTX_PSCR_PSC_SHIFT)
+    #define UARTX_PSCR_PSC_(n)  ((n)<<UARTX_PSCR_PSC_SHIFT)
 
 //寄存器基地址+偏移地址
 
 
-#endif /*_UART_H_*/
+#endif /*_UARTX_H_*/
 
 /*用于批量修改
-
+#define UARTX_(1<<)
 */
