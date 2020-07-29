cce2195bd4c9869ecd627565d3e643c190a88681
Althrone <imitatores131718@icloud.com>
2020-07-29 23:30:27 +0800

    master

--- source/include/config.h
+++ source/include/config.h
@@ -1,7 +1,7 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-    #define __STDINT_H_     //功能：定义时使用int8_t等数据类型              默认：定义
+    #define __STDINT_H_     //功能：定义时使用int8_t等数据类型             默认：定义
     #define __STM8S103_     //功能：定义芯片组，使用对应的chip 文件夹文件   默认：STM8S103
     //#define __STM8S105_
 
@@ -10,7 +10,7 @@
     #endif
     #ifdef __STM8S103_
         //#define __STM8S103F2_
-        #define __STM8S103F3_//功能：定义详细芯片
+        #define __STM8S103F3_//功能：定义详细芯片，主要是用于操作字节
         //#define __STM8S103K3_
         #include "../chip/stm8s103/stm8s103.h"
     #endif
