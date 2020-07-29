3d6bca0fdb9c5cd743a7f01dcefe6ea48395c00c
Althrone <imitatores131718@icloud.com>
2020-07-29 22:33:53 +0800

    master

--- source/chip/stm8s103/inc/option_byte.h
+++ source/chip/stm8s103/inc/option_byte.h
@@ -4,17 +4,17 @@
 #include "./memory_map.h"
 
 //寄存器偏移地址
-#define OPT_ROP_OFFSET          0x0000//Read-out protection
-#define OPT_UBC_OFFSET          0x0001//User boot code area
-#define OPT_NUBC_OFFSET         0x0002
-#define OPT_AFR_OFFSET          0x0003//Alternate function remapping
-#define OPT_NAFR_OFFSET         0x0004
-#define OPT_MISC_OFFSET         0x0005//Misc. option
-#define OPT_NMISC_OFFSET        0x0006
-#define OPT_CLK_OFFSET          0x0007//Clock option
-#define OPT_NCLK_OFFSET         0x0008
-#define OPT_HSE_OFFSET          0x0009//HSE clock startup
-#define OPT_NHSE_OFFSET         0x000A
+#define OPT_ROP_OFFSET              0x0000//Read-out protection
+#define OPT_UBC_OFFSET              0x0001//User boot code area
+#define OPT_NUBC_OFFSET             0x0002
+#define OPT_AFR_OFFSET              0x0003//Alternate function remapping
+#define OPT_NAFR_OFFSET             0x0004
+#define OPT_MISC_OFFSET             0x0005//Misc. option
+#define OPT_NMISC_OFFSET            0x0006
+#define OPT_CLK_OFFSET              0x0007//Clock option
+#define OPT_NCLK_OFFSET             0x0008
+#define OPT_HSE_OFFSET              0x0009//HSE clock startup
+#define OPT_NHSE_OFFSET             0x000A
 
 //寄存器操作
 #define OPT_ROP_ROP_SHIFT           (0)
