c849c9ff836ea1cca1ebe43b97c122d4ebbf2e5b
Althrone <imitatores131718@icloud.com>
2020-07-30 18:55:20 +0800

    master

---
+++ source/chip/stm8s103/inc/tim.h
@@ -0,0 +1,86 @@
+#ifndef _TIM_H_
+#define _TIM_H_
+
+#include "./memory_map.h"
+
+//寄存器偏移地址
+
+
+//寄存器操作
+
+//寄存器基地址+偏移地址
+#define TIM1_CR1 *(volatile unsigned char *)0x5250
+#define TIM1_CR2 *(volatile unsigned char *)0x5251
+#define TIM1_SMCR *(volatile unsigned char *)0x5252
+#define TIM1_ETR *(volatile unsigned char *)0x5253
+#define TIM1_IER *(volatile unsigned char *)0x5254
+#define TIM1_SR1 *(volatile unsigned char *)0x5255
+#define TIM1_SR2 *(volatile unsigned char *)0x5256
+#define TIM1_EGR *(volatile unsigned char *)0x5257
+#define TIM1_CCMR1 *(volatile unsigned char *)0x5258
+#define TIM1_CCMR2 *(volatile unsigned char *)0x5259
+#define TIM1_CCMR3 *(volatile unsigned char *)0x525A
+#define TIM1_CCMR4 *(volatile unsigned char *)0x525B
+#define TIM1_CCER1 *(volatile unsigned char *)0x525C
+#define TIM1_CCER2 *(volatile unsigned char *)0x525D
+#define TIM1_CNTRH *(volatile unsigned char *)0x525E
+#define TIM1_CNTRL *(volatile unsigned char *)0x525F
+#define TIM1_PSCRH *(volatile unsigned char *)0x5260
+#define TIM1_PSCRL *(volatile unsigned char *)0x5261
+#define TIM1_ARRH *(volatile unsigned char *)0x5262
+#define TIM1_ARRL *(volatile unsigned char *)0x5263
+#define TIM1_RCR *(volatile unsigned char *)0x5264
+#define TIM1_CCR1H *(volatile unsigned char *)0x5265
+#define TIM1_CCR1L *(volatile unsigned char *)0x5266
+#define TIM1_CCR2H *(volatile unsigned char *)0x5267
+#define TIM1_CCR2L *(volatile unsigned char *)0x5268
+#define TIM1_CCR3H *(volatile unsigned char *)0x5269
+#define TIM1_CCR3L *(volatile unsigned char *)0x526A
+#define TIM1_CCR4H *(volatile unsigned char *)0x526B
+#define TIM1_CCR4L *(volatile unsigned char *)0x526C
+#define TIM1_BKR *(volatile unsigned char *)0x526D
+#define TIM1_DTR *(volatile unsigned char *)0x526E
+#define TIM1_OISR *(volatile unsigned char *)0x526F
+
+/* Note these are for STM8S103 and STM8S003
+   STM8S105,104/207/208 are different */
+#define TIM2_CR1 *(volatile unsigned char *)0x5300
+#define TIM2_CR2 *(volatile unsigned char *)0x5301
+#define TIM2_SMCR *(volatile unsigned char *)0x5302
+#define TIM2_IER *(volatile unsigned char *)0x5303
+#define TIM2_SR1 *(volatile unsigned char *)0x5304
+#define TIM2_SR2 *(volatile unsigned char *)0x5305
+#define TIM2_EGR *(volatile unsigned char *)0x5306
+#define TIM2_CCMR1 *(volatile unsigned char *)0x5307
+#define TIM2_CCMR2 *(volatile unsigned char *)0x5308
+#define TIM2_CCMR3 *(volatile unsigned char *)0x5309
+#define TIM2_CCER1 *(volatile unsigned char *)0x530A
+#define TIM2_CCER2 *(volatile unsigned char *)0x530B
+#define TIM2_CNTRH *(volatile unsigned char *)0x530C
+#define TIM2_CNTRL *(volatile unsigned char *)0x530D
+#define TIM2_PSCR *(volatile unsigned char *)0x530E
+#define TIM2_ARRH *(volatile unsigned char *)0x530F
+#define TIM2_ARRL *(volatile unsigned char *)0x5310
+#define TIM2_CCR1H *(volatile unsigned char *)0x5311
+#define TIM2_CCR1L *(volatile unsigned char *)0x5312
+#define TIM2_CCR2H *(volatile unsigned char *)0x5313
+#define TIM2_CCR2L *(volatile unsigned char *)0x5314
+#define TIM2_CCR3H *(volatile unsigned char *)0x5315
+#define TIM2_CCR3L *(volatile unsigned char *)0x5316
+
+/* Note these are for STM8S103 and STM8S003
+   STM8S105,104/207/208 are different */
+#define TIM4_CR1 *(volatile unsigned char *)0x5340
+//#define TIM4_CR2 *(volatile unsigned char *)0x5341
+//#define TIM4_SMCR *(volatile unsigned char *)0x5342
+#define TIM4_IER *(volatile unsigned char *)0x5343
+#define TIM4_SR *(volatile unsigned char *)0x5344
+#define TIM4_EGR *(volatile unsigned char *)0x5345
+#define TIM4_CNTR *(volatile unsigned char *)0x5346
+#define TIM4_PSCR *(volatile unsigned char *)0x5347
+#define TIM4_ARR *(volatile unsigned char *)0x5348
+
+#endif /*_TIM_H_*/
+
+/*用于批量修改
+*/
--- source/chip/stm8s103/stm8s103.h
+++ source/chip/stm8s103/stm8s103.h
@@ -1,9 +1,10 @@
 #ifndef _STM8S103_H_
 #define _STM8S103_H_
 
-#include "./inc/gpio.h"
-#include "./inc/uart.h"
 #include "./inc/option_byte.h"
 #include "./inc/itc.h"
+#include "./inc/gpio.h"
+#include "./inc/tim.h"
+#include "./inc/uart.h"
 
 #endif /*_STM8S103_H_*/
