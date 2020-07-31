b2a537498c9803756ea6445ce16891eaec7f22ab
Althrone <imitatores131718@icloud.com>
2020-07-31 23:17:04 +0800

    master

---
+++ source/chip/stm8s103/inc/spi.h
@@ -0,0 +1,15 @@
+#ifndef _SPI_H_
+#define _SPI_H_
+
+#include "./memory_map.h"
+
+//寄存器偏移地址
+
+//寄存器操作
+
+//寄存器基地址+偏移地址
+
+#endif /*_SPI_H_*/
+
+/*用于批量修改
+*/
--- source/chip/stm8s103/stm8s103.h
+++ source/chip/stm8s103/stm8s103.h
@@ -1,13 +1,15 @@
 #ifndef _STM8S103_H_
 #define _STM8S103_H_
 
-#include "./inc/option_byte.h"
-#include "./inc/itc.h"
-#include "./inc/rst.h"
-#include "./inc/clk.h"
-#include "./inc/gpio.h"
-#include "./inc/beep.h"
-#include "./inc/tim.h"
-#include "./inc/uart.h"
+#include "./inc/option_byte.h"  //操作字节
+#include "./inc/itc.h"          //中断控制
+#include "./inc/rst.h"          //重启
+#include "./inc/clk.h"          //时钟控制
+#include "./inc/gpio.h"         //IO口
+#include "./inc/beep.h"         //蜂鸣器
+#include "./inc/tim.h"          //定时器
+#include "./inc/spi.h"          //spi
+#include "./inc/i2c.h"          //i2c
+#include "./inc/uart.h"         //串口
 
 #endif /*_STM8S103_H_*/
