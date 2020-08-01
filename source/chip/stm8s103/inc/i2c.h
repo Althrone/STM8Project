c4ee4f5274d779c43d730677b5624fca8e742018
Althrone <imitatores131718@icloud.com>
2020-08-01 15:48:42 +0800

    master

--- source/chip/stm8s103/inc/i2c.h
+++ source/chip/stm8s103/inc/i2c.h
@@ -4,10 +4,41 @@
 #include "./memory_map.h"
 
 //寄存器偏移地址
+#define I2C_CR1_OFFSET      0x0000
+#define I2C_CR2_OFFSET      0x0001
+#define I2C_FREQR_OFFSET    0x0002
+#define I2C_OARL_OFFSET     0x0003
+#define I2C_OARH_OFFSET     0x0004
+//#define I2C_Res_OFFSET    0x0005
+#define I2C_DR_OFFSET       0x0006
+#define I2C_SR1_OFFSET      0x0007
+#define I2C_SR2_OFFSET      0x0008
+#define I2C_SR3_OFFSET      0x0009
+#define I2C_ITR_OFFSET      0x000A
+#define I2C_CCRL_OFFSET     0x000B
+#define I2C_CCRH_OFFSET     0x000C
+#define I2C_TRISER_OFFSET   0x000D
+#define I2C_PECR_OFFSET     0x000E
 
 //寄存器操作
 
+
 //寄存器基地址+偏移地址
+#define I2C_CR1     *(volatile uint8_t *)(I2C_BASE+I2C_CR1_OFFSET)
+#define I2C_CR2     *(volatile uint8_t *)(I2C_BASE+I2C_CR2_OFFSET)
+#define I2C_FREQR   *(volatile uint8_t *)(I2C_BASE+I2C_FREQR_OFFSET)
+#define I2C_OARL    *(volatile uint8_t *)(I2C_BASE+I2C_OARL_OFFSET)
+#define I2C_OARH    *(volatile uint8_t *)(I2C_BASE+I2C_OARH_OFFSET)
+//#define I2C_Res   *(volatile uint8_t *)(I2C_BASE+I2C_Res_OFFSET)
+#define I2C_DR      *(volatile uint8_t *)(I2C_BASE+I2C_DR_OFFSET)
+#define I2C_SR1     *(volatile uint8_t *)(I2C_BASE+I2C_SR1_OFFSET)
+#define I2C_SR2     *(volatile uint8_t *)(I2C_BASE+I2C_SR2_OFFSET)
+#define I2C_SR3     *(volatile uint8_t *)(I2C_BASE+I2C_SR3_OFFSET)
+#define I2C_ITR     *(volatile uint8_t *)(I2C_BASE+I2C_ITR_OFFSET)
+#define I2C_CCRL    *(volatile uint8_t *)(I2C_BASE+I2C_CCRL_OFFSET)
+#define I2C_CCRH    *(volatile uint8_t *)(I2C_BASE+I2C_CCRH_OFFSET)
+#define I2C_TRISER  *(volatile uint8_t *)(I2C_BASE+I2C_TRISER_OFFSET)
+#define I2C_PECR    *(volatile uint8_t *)(I2C_BASE+I2C_PECR_OFFSET)
 
 #endif /*_I2C_H_*/
 
--- source/chip/stm8s103/inc/spi.h
+++ source/chip/stm8s103/inc/spi.h
@@ -4,16 +4,74 @@
 #include "./memory_map.h"
 
 //寄存器偏移地址
-#define SPI_CR1_OFFSET      0x0000
-#define SPI_CR2_OFFSET      0x0001
-#define SPI_ICR_OFFSET      0x0002
-#define SPI_SR_OFFSET       0x0003
-#define SPI_DR_OFFSET       0x0004
-#define SPI_CRCPR_OFFSET    0x0005
-#define SPI_RXCRCR_OFFSET   0x0006
-#define SPI_TXCRCR_OFFSET   0x0007
+#define SPI_CR1_OFFSET              0x0000
+#define SPI_CR2_OFFSET              0x0001
+#define SPI_ICR_OFFSET              0x0002
+#define SPI_SR_OFFSET               0x0003
+#define SPI_DR_OFFSET               0x0004
+#define SPI_CRCPR_OFFSET            0x0005
+#define SPI_RXCRCR_OFFSET           0x0006
+#define SPI_TXCRCR_OFFSET           0x0007
 
 //寄存器操作
+#define SPIX_CR1_CPHA               (1<<0)
+#define SPIX_CR1_CPOL               (1<<1)
+#define SPIX_CR1_MSTR               (1<<2)
+#define SPIX_CR1_BR_SHIFT           (3)
+#define SPIX_CR1_BR_MASK            (0x07<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV2        (0x00<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV4        (0x01<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV8        (0x02<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV16       (0x03<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV32       (0x04<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV64       (0x05<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV128      (0x06<<SPIX_CR1_BR_SHIFT)
+    #define SPIX_CR1_BR_DIV256      (0x07<<SPIX_CR1_BR_SHIFT)
+#define SPIX_CR1_SPE                (1<<6)
+#define SPIX_CR1_LSBFIRST           (1<<7)
+
+#define SPIX_CR2_SSI                (1<<0)
+#define SPIX_CR2_SSM                (1<<1)
+#define SPIX_CR2_RXONLY             (1<<2)
+//#define SPIX_CR2_Res              (1<<3)
+#define SPIX_CR2_CRCNEXT            (1<<4)
+#define SPIX_CR2_CRCEN              (1<<5)
+#define SPIX_CR2_BDOE               (1<<6)
+#define SPIX_CR2_BDM                (1<<7)
+
+//#define SPIX_ICR_Res              (1<<0)
+//#define SPIX_ICR_Res              (1<<1)
+//#define SPIX_ICR_Res              (1<<2)
+//#define SPIX_ICR_Res              (1<<3)
+#define SPIX_ICR_WKIE               (1<<4)
+#define SPIX_ICR_ERRIE              (1<<5)
+#define SPIX_ICR_RXIE               (1<<6)
+#define SPIX_ICR_TXIE               (1<<7)
+
+#define SPIX_SR_RXNE                (1<<0)
+#define SPIX_SR_TXE                 (1<<1)
+//#define SPIX_SR_Res               (1<<2)
+#define SPIX_SR_WKUP                (1<<3)
+#define SPIX_SR_CRCERR              (1<<4)
+#define SPIX_SR_MODF                (1<<5)
+#define SPIX_SR_OVR                 (1<<6)
+#define SPIX_SR_BSY                 (1<<7)
+
+#define SPIX_DR_DR_SHIFT            (0)
+#define SPIX_DR_DR_MASK             (0xFF<<SPIX_DR_DR_SHIFT)
+    #define SPIX_DR_DR_(n)          ((n)<<SPIX_DR_DR_SHIFT)
+
+#define SPIX_CRCPR_CRCPOLY_SHIFT    (0)
+#define SPIX_CRCPR_CRCPOLY_MASK     (0xFF<<SPIX_CRCPR_CRCPOLY_SHIFT)
+    #define SPIX_CRCPR_CRCPOLY_(n)  ((n)<<SPIX_CRCPR_CRCPOLY_SHIFT)
+
+#define SPIX_RXCRCR_RXCRC_SHIFT     (0)
+#define SPIX_RXCRCR_RXCRC_MASK      (0xFF<<SPIX_RXCRCR_RXCRC_SHIFT)
+    #define SPIX_RXCRCR_RXCRC_(n)   ((n)<<SPIX_RXCRCR_RXCRC_SHIFT)
+
+#define SPIX_TXCRCR_TXCRC_SHIFT     (0)
+#define SPIX_TXCRCR_TXCRC_MASK      (0xFF<<SPIX_TXCRCR_TXCRC_SHIFT)
+    #define SPIX_TXCRCR_TXCRC_(n)   ((n)<<SPIX_TXCRCR_TXCRC_SHIFT)
 
 //寄存器基地址+偏移地址
 #define SPI_CR1     *(volatile uint8_t *)(SPI_BASE+SPI_CR1_OFFSET)
